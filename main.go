43bd49943ba7f46a7bf8e51c9ef13b1313fe5004
johoermann <johannes.hoermann@tiffinger-thiel.de>
2021-11-11 13:14:37 +0100

    Add Interfaces

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type FileLoader interface {
+}
+
+type TagFinder interface {
+}
+
+type CommentCleaner interface {
+}
+
+type Generator interface {
+}
+
+type Writer interface {
+}
+
 func main() {
 	fmt.Println("Hello World")
 }
