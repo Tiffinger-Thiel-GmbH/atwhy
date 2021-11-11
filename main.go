bfc052aa76629db7a9ad052cb05fbd582d3e5ea2
niklas_t <n.thiem@tiffinger-thiel.de>
2021-11-11 14:01:19 +0100

    feat: added functionality definition for TagFinder

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+)
 
 type FileLoader interface {
 }
 
+type TagType string
+
+var (
+	Why TagType = "WHY"
+	Readme TagType = "README"
+	FileLine TagType = "FILELINE"
+)
+
+type Tag struct {
+	Filename string
+	Line int
+	Value string
+}
+
 type TagFinder interface {
+	Find(filename string, reader io.Reader) ([]Tag, error)
+	// SaveByTag()
+	// scan()
+	// findTag()
+	// saveByTag()
 }
 
 type CommentCleaner interface {
@@ -19,4 +41,5 @@ type Writer interface {
 
 func main() {
 	fmt.Println("Hello World")
+
 }
