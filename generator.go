ad64b0e08ee011b77bbc2cd99c47f2a00ea7f6fc
Johannes Hertrich <johannes.hertrich@tiffinger-thiel.de>
2021-11-18 15:15:44 +0100

    Add bootstrap

--- generator.go
+++ generator.go
@@ -64,6 +64,20 @@ func (hg HTMLGenerator) Generate(tags []tag.Tag, writer io.Writer) error {
 
 	gm := goldmark.New()
 
+	writer.Write([]byte(`<head>
+    <meta charset="utf-8">
+    <title>CrazyDoc</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <style>
+      
+    </style>
+  </head>
+  <body>`))
+
 	err = gm.Convert([]byte(res.String()), writer)
+
+	writer.Write([]byte(`</body>`))
+
 	return err
 }
